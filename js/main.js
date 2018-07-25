f7d05e10fabfd3cf37cfb1cabf5a4a873ef0b05a
Slawomir Wojda <slwojda@poczta.onet.pl>
2018-07-25 15:54:26 +0200

    funkcja dodawanie

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let liczbaPierwsza;
+let liczbaDruga;
+
+liczbaPierwsza = 10;
+liczbaDruga = 10;
+
+
+function dodawanie(parametr1, paramert2) {
+    let suma = parametr1 + paramert2;
+    return suma;
+}
+
+console.log(dodawanie(liczbaPierwsza, liczbaDruga));
